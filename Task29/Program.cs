62d23a83dfd29f24915a5703165c58e8de63bc24
Yana <leduschenkova6@gmail.com>
2022-10-03 14:53:21 +0300

    убрали инициализацию i

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,9 +3,8 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 int[] array = new int[8];
-int i = 0;
 
-for(i = 0; i < array.Length; i++)
+for(int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(1,100);
         Console.Write($"{array[i]} ");
